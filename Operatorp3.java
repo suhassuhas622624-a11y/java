3f55dc764dd57a25ac17887891477a04866fbc62
suhassuhas622624-a11y <suhassuhas622624@gmail.com>
2026-09-08 08:12:22 +0530

    Add files via upload

---
+++ Operatorp3.java
@@ -0,0 +1,11 @@
+public class Operatorp3 {
+
+        public static void main(String [] args){
+        int x=30;
+        float y;
+        y=x;
+        System.out.println("int:"+x);
+        System.out.println("float:"+y);
+    }
+
+}
