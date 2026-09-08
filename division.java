a012ef654d27650db4207ec1696a417e18ce3e2a
suhassuhas622624-a11y <suhassuhas622624@gmail.com>
2026-09-08 08:01:06 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,12 @@
+public class division{
+    public static void main(String[] args) {
+        int num = 221;
+        if (num % 13 == 0 ) {
+            System.out.println("221 is divisible by 13");
+        }
+        if (num % 17 == 0){
+            System.out.println("221 is divisible by 17");
+        
+        }
+    }
+}
