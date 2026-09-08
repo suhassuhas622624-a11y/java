d2e536abc5fa8c2824414b32098f424b31911a40
suhassuhas622624-a11y <suhassuhas622624@gmail.com>
2026-09-08 08:00:12 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        int x = 30;
+        float y;
+        y=x;
+        System.out.println("int: "+x);
+        System.out.println("float: "+y);
+    }
+}
